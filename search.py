7fe761fee928ce43f368bb85f79db8115d9f0a7f
KingRift <kingrift56@gmail.com>
2022-05-19 19:52:47 +0300

    Initial commit

---
+++ search.py
@@ -0,0 +1,32 @@
+from AnilistPython import Anilist
+
+anilist = Anilist()
+
+# Input
+def input_checker(text, options):
+    option = int(input(text))
+    if option not in options:
+        print('Invalid input!')
+        return input_checker(text, options)
+    else:
+        return option
+
+# Creates the list of possible options
+def create_list(options):
+    options_list = []
+    for i in range(len(options)):
+        options_list.append('[%s] %s' % (i, options[i]))
+    return options_list
+
+
+# Menu
+def menu(options_list):
+    correct_options = []
+    print('<Weeb Search>')
+    print('')
+    options = create_list(options_list)
+    for i in range(len(options)):
+        correct_options.append(i)
+        print(options[i])
+    option = input_checker('Your choice: ', correct_options)
+    

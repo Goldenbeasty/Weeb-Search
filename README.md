8cba2203c24a361c8ec12c07dece7c7e4466984c
KingRift <kingrift56@gmail.com>
2022-05-22 15:48:24 +0300

    Finished project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rift's Weeb Search
+
+A small program that searches for information on anime, manga and characters from AniList, using [AnilistPython](https://github.com/ReZeroE/AnilistPython).
--- search.py
+++ search.py
@@ -1,6 +1,65 @@
 from AnilistPython import Anilist
+import os
 
 anilist = Anilist()
+
+### CLASSES
+class Anime:
+    def __init__(self, name):
+        data = anilist.get_anime(name)
+        self.name_romaji = data['name_romaji']
+        self.name_english = data['name_english']
+        self.genres = data['genres']
+        self.episodes = data['airing_episodes']
+        self.desc = data['desc']
+
+    def display(self):
+        print('Name (Romaji): ' + str(self.name_romaji))
+        print('Name (English): ' + str(self.name_english))
+        print('Genres: ' + str(self.genres))
+        print('Episodes: ' + str(self.episodes))
+        print('Description: ' + str(self.desc))
+
+class Manga:
+    def __init__(self, name):
+        data = anilist.get_manga(name)
+        self.name_romaji = data['name_romaji']
+        self.name_english = data['name_english']
+        self.genres = data['genres']
+        self.release_format = data['release_format']
+        self.release_status = data['release_status']
+        self.volumes = data['volumes']
+        self.chapters = data['chapters']
+        self.desc = data['desc']
+    
+    def display(self):
+        print('Name (English): ' + str(self.name_english))
+        print('Name (Romaji): ' + str(self.name_romaji))
+        print('Genres: ' + str(self.genres))
+        print('Format: ' + str(self.release_format))
+        print('Status: ' + str(self.release_status))
+        print('Volumes: ' + str(self.volumes))
+        print('Chapters: ' + str(self.chapters))
+        print('Description: ' + str(self.desc))
+
+class Character:
+    def __init__(self, name):
+        data = anilist.get_character(name)
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.native_name = data['native_name']
+        self.desc = data['desc']
+        self.image = data['image']
+
+    def display(self):
+        print('First name: ' + str(self.first_name))
+        print('Last name: ' + str(self.last_name))
+        print('Native name: ' + str(self.native_name))
+        print('Description: ' + str(self.desc))
+        print('Image: ' + str(self.image))
+
+
+### USER INTERACTION
 
 # Input
 def input_checker(text, options):
@@ -21,16 +80,35 @@ def create_list(options):
 # Menu
 def menu(options_list):
     correct_options = []
-    print("<Rift's Weeb Search>/n")
+    print("<Rift's Weeb Search>")
+    print('')
     options = create_list(options_list)
     for i in range(len(options)):
         correct_options.append(i)
         print(options[i])
     option = input_checker('Your choice: ', correct_options)
+    return option
 
 
-# Main Menu
-def main_menu():
-    menu(['Anime', 'Manga', 'Character'])
+### Program 'loop'
+def run():
+    option = menu(['Anime', 'Manga', 'Character'])
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-main_menu()
+    print("<Rift's Weeb Search>")
+    print()
+    search = input('Name: ')
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print("<Rift's Weeb Search>")
+    print()
+    
+    if option == 0:
+        o = Anime(search)
+    elif option == 1:
+        o = Manga(search)
+    elif option == 2:
+        o = Character(search)
+    o.display()
+
+run()
